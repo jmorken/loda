2771f2d97dde742fd5e5318d334483cd0453560b
Jamie <jmorken@shaw.ca>
2021-04-27 12:52:14 -0700

    Add files via upload

--- programs/oeis/192/A192920.asm
+++ programs/oeis/192/A192920.asm
@@ -1,43 +1,32 @@
 ; A192920: Coefficient of x in the reduction by (x^2 -> x+1) of the polynomial F(n+4)*x^n, where F=A000045 (Fibonacci sequence).
 ; 0,5,8,26,63,170,440,1157,3024,7922,20735,54290,142128,372101,974168,2550410,6677055,17480762,45765224,119814917,313679520,821223650,2149991423,5628750626,14736260448,38580030725,101003831720,264431464442,692290561599,1812440220362,4745030099480,12422650078085,32522920134768,85146110326226,222915410843903,583600122205490,1527884955772560,4000054745112197
 
-mov $17,$0
-mov $19,2
-lpb $19
-  mov $0,$17
-  sub $19,1
-  add $0,$19
+mov $5,$0
+mov $7,2
+lpb $7
+  clr $0,5
+  mov $0,$5
+  sub $7,1
+  add $0,$7
   sub $0,1
-  mov $3,$0
-  add $3,24
-  mov $5,$0
-  div $5,2
-  mul $5,2
-  sub $5,$3
-  cal $0,119996 ; Numerator of Sum_{k=1..n} 1/(Fibonacci(k)*Fibonacci(k+2)).
-  sub $0,$5
+  max $0,0
+  cal $0,80143 ; a(n) = F(3)*F(n)*F(n+1) + F(4)*F(n+1)^2 - F(4) if n even, F(3)*F(n)*F(n+1) + F(4)*F(n+1)^2 if n odd, where F(n) is the n-th Fibonacci number (A000045).
   mov $1,$0
-  add $2,7
-  lpb $4,13
-    sub $1,2
-    mul $2,2
-    sub $2,5
-    lpb $2
-      add $1,11
-      sub $2,9
-      mov $5,2
-    lpe
-    sub $1,$5
-  lpe
-  sub $1,32
-  mov $20,$19
-  lpb $20
-    mov $18,$1
-    sub $20,1
-  lpe
+  add $1,3
+  trn $1,8
+  mov $1,$0
+  mov $1,$0
+  add $3,2
+  mov $4,$0
+  sub $4,$0
+  trn $4,$3
+  mul $3,2
+  mov $8,$7
+  cmp $8,1
+  mul $8,$0
+  add $6,$8
 lpe
-lpb $17
-  mov $17,0
-  sub $18,$1
-lpe
-mov $1,$18
+min $5,1
+mul $5,$1
+mov $1,$6
+sub $1,$5
