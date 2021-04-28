f5577a964d770da7a9cabf87ed5dc6f1da574262
Jamie <jmorken@shaw.ca>
2021-04-27 17:04:41 -0700

    Add files via upload

--- programs/oeis/158/A158948.asm
+++ programs/oeis/158/A158948.asm
@@ -8,16 +8,15 @@ lpb $3
   sub $3,1
   add $0,$3
   sub $0,1
-  cal $0,4202 ; Skip 1, take 1, skip 2, take 2, skip 3, take 3, etc.
+  max $0,0
+  cal $0,64801 ; Take 1, skip 2, take 2, skip 3, take 3, etc.
+  add $0,1
   div $0,2
   mov $2,$3
+  mul $2,$0
+  add $1,$2
   mov $4,$0
-  lpb $2
-    mov $1,$4
-    sub $2,1
-  lpe
 lpe
-lpb $5
-  sub $1,$4
-  mov $5,0
-lpe
+min $5,1
+mul $5,$4
+sub $1,$5
